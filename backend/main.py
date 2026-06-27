80e00b5a0a80ad53b5519b0b8e78876f1931d8ee
Ketan Pawar <pawarketan431@gmail.com>
2026-06-27 09:36:26 +0530

    Allow all Vercel preview URLs in CORS

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", 
                    "http://127.0.0.1:5173"
-                   "https://task-board-ketan75.vercel.app"],
+                   "https://task-board-ketan75.vercel.app",
+                   "https://task-board-git-main-ketan75.vercel.app",
+                   "https://task-board-qedf8b5rn-ketan75.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
